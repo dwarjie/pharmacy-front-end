32c41fea86896ef189b1da09c884a9fc37e6afe2
unknown <macoycoder@gmail.com>
2023-01-07 00:58:22 +0800

    Changed base url

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "http://localhost:8080/api",
+	// baseURL: "http://localhost:8080/api",
+	baseURL: "https://pharmacy-management-system.herokuapp.com/api",
 	headers: {
 		ContentType: "application/json",
 	},
